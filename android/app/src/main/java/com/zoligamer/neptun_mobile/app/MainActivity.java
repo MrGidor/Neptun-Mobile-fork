ed3f48b73e7ab9131ab06ad1ba9caf4ac9bde913
Gidor <gidorstudios.help@gmail.com>
2026-05-20 16:31:30 +0200

    fix: missing MainAcivity.java file.

---
+++ android/app/src/main/java/com/zoligamer/neptun_mobile/app/MainActivity.java
@@ -0,0 +1,22 @@
+package com.zoligamer.neptun_mobile.app;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
